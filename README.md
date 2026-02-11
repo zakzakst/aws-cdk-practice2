83396d1de1b744a766172089773a27bc4c8c3072
zakzakst <e1433104@gmail.com>
2026-02-12 07:09:45 +0900

    fix

--- README.md
+++ README.md
@@ -11,3 +11,11 @@ cdk cli-telemetry --disable
 
 AWS_PROFILE=admin cdk migrate --stack-name template --from-path ./template.yaml -l typescript
 ```
+
+## IaCジェネレータ ダウンロードメモ
+
+- 0205: 1つのEC2インスタンスのIDで検索してヒットしたものからテンプレート作成
+- 0205-2: 1つのVPCのIDで検索してヒットしたものからテンプレート作成
+  - 0205でVPC連携でエラー出たのでVPCでテンプレート作成してみたが、結局VPCのIDでエラー出た
+- 0212: 1つのS3のIDで検索してヒットしたものからテンプレート作成
+  - resourceのところでエラーが出た
--- ec2/lib/ec2-stack.ts
+++ ec2/lib/ec2-stack.ts
@@ -1,10 +1,23 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib/core";
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 
+export interface Ec2StackProps extends cdk.StackProps {
+  /**
+   * Name of an existing EC2 KeyPair to enable SSH access
+   * @default 'my-cdk-ec2'
+   */
+  readonly keyName?: string;
+}
 export class Ec2Stack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: Ec2StackProps) {
     super(scope, id, props);
+
+    // デフォルトのpropsを設定
+    props = {
+      ...props,
+      keyName: props.keyName ?? "my-cdk-ec2",
+    };
 
     // The code that defines your stack goes here
 
